59567ac3a9e00cf1370553bed1c79e3cf0aae6ef
Murat <murat199611@mail.ru>
2021-05-07 13:57:37 +0300

    fix focus

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import Section from '../Section/Section';
 import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
-import submit from '../../images/submit.svg';
 import './SearchForm.css';
 function SearchForm() {
   return (
     <Section className="search-form" tablet="s" phone="m">
       <form className="search-form__form">
-        <input className="search-form__input" placeholder="Фильм" />
-        <button className="search-form__submit" type="submit">
-          <img src={submit} className="search-form__submit-img" />
-        </button>
+        <input className="search-form__input" placeholder="Фильм" required />
+        <button className="search-form__submit" type="submit"></button>
         <FilterCheckbox />
         <div className="search-form__border" />
       </form>
